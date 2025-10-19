b6e67f6e9bd90a768c1a51915b0adb16904b23d1
raisfordiner <truongkienthanh6705@gmail.com>
2025-10-19 20:46:01 +0700

    devops: hotfix: eslint and ci

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,17 +1,26 @@
 import js from "@eslint/js";
 import globals from "globals";
+import prettierConfig from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
-    extends: ["js/recommended", "prettier"],
-    languageOptions: { globals: globals.browser }
-  }, {
+    extends: [js.configs.recommended, prettierConfig],
+    languageOptions: { 
+        globals: globals.node,
+        sourceType: "commonjs"
+    },
+  },
+  {
     rules: {
-      "no-unused-vars": "warn",
+      "no-unused-vars": [
+          "warn",
+          { "argsIgnorePattern": "req|res" }
+      ],
       "no-undef": "warn",
     },
   },
 ]);
+
