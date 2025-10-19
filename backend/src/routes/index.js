6aeabdcf5d2b9fc250f34f5d57923432d0ae832b
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-10-19 07:34:39 +0700

    Add Prisma

--- backend/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.use('/', require('./defaultRoutes'))
-router.use('/dummies', require('./dummyRoutes'))
-
-module.exports = router
