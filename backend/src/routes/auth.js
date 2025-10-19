124964e60771626d1c6ededd82fac45e9ac060bb
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-10-19 08:35:58 +0700

    Add Swagger and routes

---
+++ backend/src/routes/auth.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+
+router.post("/signup", createAccount);
+
+router.post("/login", loginUser);
+
+router.post("/forgot-password", forgotPassword);
+
+// token for reset password
+router.post("/check-token", verifyResetToken);
+
+router.post("/reset-password", resetPassword);
+
+router.post("/refresh-token", refreshToken);
+
+module.exports = router;
