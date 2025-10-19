b5517b268cfcb35393837982fbf6159589801ccf
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-10-19 08:35:58 +0700

    Add Swagger and routes

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
-
-router.get("/", (req, res) => {
-    res.send("All authors")
-});
+const verifyToken = require("../middleware/verifyToken");
 
 router.use(verifyToken);
+
 router.route("/").get(verifyAdmin, getAllUsers).post(verifyAdmin, createUser);
+
 router.route("/profile").get(getUserProfile);
+
 router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
 module.exports = router;
