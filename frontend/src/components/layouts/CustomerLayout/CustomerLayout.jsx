02616ca1b93d9cff838051f196f989dc511d7c7b
Truong Quoc Thong <truongquocthong07.14@gmail.com>
2025-10-30 19:40:37 +0700

    feat: set basic layouts for customer and admin, add login and register pages

--- frontend/src/components/layouts/CustomerLayout/CustomerLayout.jsx
+++ frontend/src/components/layouts/CustomerLayout/CustomerLayout.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import {Outlet} from "react-router-dom"
+import {Layout} from "antd";
+const { Header, Footer, Content } = Layout;
+
 
 const CustomerLayout = () => {
   return (
     <>
-      CustomerLayout
-      <Outlet />
+      <Layout>
+          <header>
+              Header
+          </header>
+          <Content style={{ padding: '80px 120px' }}>
+              <Outlet />
+          </Content>
+          <Footer>
+              The everything shop ©2025
+          </Footer>
+      </Layout>
     </>
   )
 }
