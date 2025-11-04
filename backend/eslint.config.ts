96f338c02699c2a5faa64b3981dfff2b779197d0
raisfordiner <truongkienthanh6705@gmail.com>
2025-11-04 22:23:36 +0700

    fix: eslint

--- backend/eslint.config.ts
+++ backend/eslint.config.ts
@@ -5,22 +5,15 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: [js.configs.recommended, prettierConfig],
-    languageOptions: { 
-        globals: globals.node,
-        sourceType: "commonjs"
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: globals.node,
+      sourceType: "commonjs",
     },
-  },
-  {
+    extends: [js.configs.recommended, prettierConfig],
     rules: {
-      "no-unused-vars": [
-          "warn",
-          { "argsIgnorePattern": "req|res" }
-      ],
+      "no-unused-vars": ["warn", { argsIgnorePattern: "req|res" }],
       "no-undef": "warn",
     },
   },
 ]);
-
