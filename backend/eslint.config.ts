439d4829b7b9d6caa2fdc4f1dbde7f010e61b6a3
raisfordiner <truongkienthanh6705@gmail.com>
2025-11-04 22:35:21 +0700

    fix: eslint again

--- backend/eslint.config.ts
+++ backend/eslint.config.ts
@@ -1,13 +1,17 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettierConfig from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
+  js.config.recommended,
+  tseslint.configs.recommended,
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
     languageOptions: {
       globals: globals.node,
+      parser: tseslint.parser,
       sourceType: "module",
     },
     extends: [js.configs.recommended, prettierConfig],
