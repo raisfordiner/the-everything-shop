42b260102ff31d5ae25b5a6ea6a5e505357c7e4e
Hoang Phuc <49684127+Huangphoux@users.noreply.github.com>
2025-11-11 21:02:28 +0700

    fix: Users API
    - Merge /search into /users

--- backend/src/users/users.schema.ts
+++ backend/src/users/users.schema.ts
@@ -1,24 +1,5 @@
 import { z } from "zod";
-
-const passwordSchema = z
-  .string()
-  .min(8, "Password must be at least 8 characters long")
-  .regex(/[A-Z]/, "Password must include at least one uppercase letter")
-  .regex(/[a-z]/, "Password must include at least one lowercase letter")
-  .regex(/[0-9]/, "Password must include at least one number")
-  .regex(/[@$!%*?&]/, "Password must include at least one special character");
-
-const usernameSchema = z
-  .string()
-  .min(6, "Username must be at least 6 characters long")
-  .max(20, "Username must not exceed 20 characters")
-  .regex(/^[a-zA-Z0-9_-]+$/, "Username can only contain letters, numbers, hyphens, and underscores")
-  .refine((value) => !/^\d+$/.test(value), {
-    message: "Username cannot be only numbers",
-  })
-  .refine((value) => !/[@$!%*?&]/.test(value), {
-    message: "Username cannot contain special characters like @$!%*?&",
-  });
+import { usernameSchema, passwordSchema } from "auth/auth.schema";
 
 const roleSchema = z.enum(["CUSTOMER", "SELLER", "ADMIN"], "Invalid role");
 
@@ -30,14 +11,15 @@ const create = z.object({
 });
 
 const update = z.object({
-  username: usernameSchema,
-  email: z.email("Invalid email format"),
-  role: roleSchema,
+  username: usernameSchema.optional(),
+  email: z.email("Invalid email format").optional(),
+  password: passwordSchema.optional(),
+  role: roleSchema.optional(),
 });
 
 const search = z.object({
-  q: z.string(),
-  role: roleSchema.optional,
+  q: z.string().optional(),
+  role: roleSchema.optional(),
 });
 
 const UsersSchema = { create, update, search };
