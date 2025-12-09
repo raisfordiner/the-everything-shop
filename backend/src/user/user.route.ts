27246e75a64264e668c3b12327477315eaf90923
raisfordiner <truongkienthanh6705@gmail.com>
2025-12-09 22:21:53 +0700

    feat: user pfp

--- backend/src/user/user.route.ts
+++ backend/src/user/user.route.ts
@@ -1,6 +1,15 @@
 import BaseRouter, { RouteConfig } from "util/router";
 import AuthMiddleware from "auth/auth.middleware";
 import UserController from "./user.controller";
+import multer from 'multer';
+
+// Configure multer
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB limit for PFP
+  },
+});
 
 class UserRoutes extends BaseRouter {
   protected routes(): RouteConfig[] {
@@ -16,6 +25,18 @@ class UserRoutes extends BaseRouter {
         path: "/info",
         middlewares: [AuthMiddleware.authenticateUser],
         controller: UserController.updateUser,
+      },
+      {
+        method: "get",
+        path: "/pfp",
+        middlewares: [AuthMiddleware.authenticateUser],
+        controller: UserController.getPfp,
+      },
+      {
+        method: "post",
+        path: "/pfp",
+        middlewares: [AuthMiddleware.authenticateUser, upload.single('file')],
+        controller: UserController.uploadPfp,
       },
     ];
   }
