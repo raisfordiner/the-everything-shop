921215aea9e1d0e1d90131b18ab652270bf0cb73
Truong Quoc Thong <truongquocthong07.14@gmail.com>
2025-12-21 20:50:40 +0700

    feat: add promotions page for admin portal

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -40,6 +40,10 @@ import EditStaff from "../pages/admin/Staffs/EditStaff.jsx";
 import AddStaff from "../pages/admin/Staffs/AddStaff.jsx";
 import AllProducts from '../pages/customer/AllProducts/AllProducts.jsx';
 import SearchProduct from '../pages/customer/SearchProduct/SearchProduct.jsx';
+import AdminReports from "../pages/admin/Reports/Reports.jsx";
+import Promotions from "../pages/admin/Promotions/Promotions.jsx";
+import EditPromotion from "../pages/admin/Promotions/EditPromotion.jsx";
+import AddPromotion from "../pages/admin/Promotions/AddPromotion.jsx";
 
 export const routes = [
     {
@@ -144,25 +148,24 @@ export const routes = [
             <ProtectedRoute allowedRoles={['ADMIN']}>
                 <AdminLayout />
             </ProtectedRoute>
-            // <AdminLayout /> 
         ),
         children: [
-            {
-                path: "dashboard",
-                element: <DashBoard />
-            },
-            {
-                path: "categories",
-                element: <Category />
-            },
-            {
-                path: "categories/add-category",
-                element: <AddCategory />
-            },
-            {
-                path: "categories/edit-category/:id",
-                element: <EditCategory />
-            },
+            // {
+            //     path: "dashboard",
+            //     element: <DashBoard />
+            // },
+            // {
+            //     path: "categories",
+            //     element: <Category />
+            // },
+            // {
+            //     path: "categories/add-category",
+            //     element: <AddCategory />
+            // },
+            // {
+            //     path: "categories/edit-category/:id",
+            //     element: <EditCategory />
+            // },
             {
                 path: "orders",
                 element: <Order />,
@@ -192,6 +195,18 @@ export const routes = [
                 element: <AddCustomer />
             },
             {
+                path: "promotions",
+                element: <Promotions/>,
+            },
+            {
+                path: "promotions/edit-promotion/:id",
+                element: <EditPromotion />,
+            },
+            {
+                path: "promotions/add-promotion",
+                element: <AddPromotion />,
+            },
+            {
                 path: "coupons",
                 element: <Coupons />,
             },
@@ -202,6 +217,10 @@ export const routes = [
             {
                 path: "coupons/add-coupon",
                 element: <AddCoupon />
+            },
+            {
+                path: "reports",
+                element: <AdminReports />,
             }
         ]
     },
