ec56278ffb6568d74b701c6690e772ab2b07869a
Truong Quoc Thong <truongquocthong07.14@gmail.com>
2025-12-27 17:33:48 +0700

    feat: implement logout for admin

--- frontend/src/components/headers/AdminHeader/AdminHeader.jsx
+++ frontend/src/components/headers/AdminHeader/AdminHeader.jsx
@@ -1,38 +1,83 @@
 import { MessageOutlined, BellOutlined, DownOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
-import { Avatar, Badge, Dropdown, Space } from "antd";
+import { Avatar, Badge, Dropdown, Space, message } from "antd";
 import "./AdminHeader.css";
 import {getAvatarColor} from "../../../utils/avatar.js";
 import React from "react";
+import authService from "../../../services/authService.js";
+import { useDispatch, useSelector } from "react-redux";
+import {setLogout} from "../../../redux/actions/authAction.js";
+import { UserOutlined, LogoutOutlined } from '@ant-design/icons'
+import { useNavigate } from "react-router";
 
 const AdminHeader = () => {
-    const items = [
-        { key: "1", label: <Link to="/logout">Logout</Link> },
-    ];
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [messageApi, contextHolder] = message.useMessage();
+    const { isAuthenticated, user } = useSelector((state) => state.authReducer);
+
+    const handleLogout = async () => {
+        try {
+            await authService.logout();
+            dispatch(setLogout());
+            messageApi.open({
+                type: 'success',
+                content: 'Log out successfully!',
+            });
+            navigate('/login');
+        }
+        catch (error) {
+            console.error(error);
+            messageApi.open({
+                type: 'error',
+                content: 'Log out failed',
+            });
+        }
+    }
+
+    const userMenu = {
+        items: [
+            {
+                key: 'logout',
+                icon: <LogoutOutlined />,
+                label: 'Logout',
+                onClick: handleLogout,
+            }
+        ]
+    };
 
     return (
         <header className="admin-header">
+            {contextHolder}
+
             <Link to="/admin/orders" className="logo">
                 ADMIN PORTAL
             </Link>
 
             <div className="header-right">
-                <Dropdown menu={{ items }} placement="bottomRight" arrow>
-                    <Space className="admin-info">
-                        <Avatar
-                            style={{
-                                backgroundColor: getAvatarColor("Admin"),
-                                verticalAlign: 'middle',
-                                color: '#fff',
-                                fontWeight: 600
-                            }}
-                        >
-                            A
-                        </Avatar>
-                        <span className="admin-name">Administrator</span>
-                        <DownOutlined className="down-icon" />
-                    </Space>
-                </Dropdown>
+                {isAuthenticated ? (
+                    <Dropdown menu={userMenu} placement="bottomRight" arrow>
+                        <Space className="admin-info">
+                            <Avatar
+                                style={{
+                                    backgroundColor: getAvatarColor(user?.username || 'Admin'),
+                                    verticalAlign: 'middle',
+                                    color: '#fff',
+                                    fontWeight: 600
+                                }}
+                            >
+                                A
+                            </Avatar>
+                            <span className="admin-name">{user?.username || 'Administrator'}</span>
+                            <DownOutlined className="down-icon" />
+                        </Space>
+                    </Dropdown>
+                ) : (
+                    <Link to="/login" className="header-action-item">
+                        <UserOutlined className="icon" />
+                        <span>Sign In</span>
+                    </Link>
+                )}
             </div>
         </header>
     );
